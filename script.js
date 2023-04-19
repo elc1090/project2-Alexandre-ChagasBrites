c4922630c525a8d4d40fbab35d038301a6c01b03
Alexandre Chagas Brites <acbrites@inf.ufsm.br>
2023-04-19 10:48:35 -0300

    Refatorado script.js e adicionado indicador de elemento com foco

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
-requestGeneration(1)
-    .then(response => response.json())
-    .then(data => {
-        let pokemons = data.pokemon_species.sort(function(a, b){
-            return getId(a) - getId(b);
-        });
+fillPokedex();
 
-        let ol = document.getElementById('pokemonSpecies');
-        for (let i in pokemons) {
-            let pokemon = data.pokemon_species[i];
+async function fillPokedex() {
+    let response = await requestGeneration(1);
+    let data = await response.json();
 
-            let id = String(getId(pokemon)).padStart(3, '0');
-            let name = pokemon.name.slice(0, 1).toUpperCase() + pokemon.name.slice(1);
-
-            let li = document.createElement('li');
-            li.classList.add('list-group-item');
-            li.innerHTML = (`
-                <p class="id">${id}</p>
-                <p class="name">${name}</p>
-            `);
-
-            ol.appendChild(li);
-        }
+    let pokemons = data.pokemon_species.sort(function(a, b){
+        return getId(a) - getId(b);
     });
 
+    let ol = document.getElementById('pokemonSpecies');
+    for (let pokemon of pokemons) {
+        let id = String(getId(pokemon)).padStart(3, '0');
+        let name = pokemon.name.slice(0, 1).toUpperCase() + pokemon.name.slice(1);
+
+        let li = document.createElement('li');
+        li.tabIndex = getId(pokemon);
+        li.innerHTML = (`
+            <p class="id">${id}</p>
+            <p><span class="arrow">▶</span>${name}</p>
+        `);
+
+        li.addEventListener('focus', (e) => {
+            let arrow = e.target.querySelector('.arrow');
+            arrow.textContent = '▷';
+        });
+
+        li.addEventListener('blur', (e) => {
+            let arrow = e.target.querySelector('.arrow');
+            arrow.textContent = '▶';
+        });
+
+        ol.appendChild(li);
+    }
+}
+
 function requestGeneration(i) {
-    return Promise.resolve(fetch(`https://pokeapi.co/api/v2/generation/${i}`));
+    return fetch(`https://pokeapi.co/api/v2/generation/${i}`);
 }
 
 function getId(a) {
-    let url = a.url.substring(0, a.url.length - 1);
-    url = url.substring(url.lastIndexOf('/') + 1);
+    let url = a.url.slice(0, a.url.length - 1);
+    url = url.slice(url.lastIndexOf('/') + 1);
     return parseInt(url);
 }
